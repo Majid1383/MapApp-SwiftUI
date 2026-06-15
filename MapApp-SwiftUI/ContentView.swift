ba2137a9b5e6c418695fc0c9298c052863d99b6f
Majid1383 <sammajid2540@gmail.com>
2026-06-15 18:28:16 +0530

    Initial Commit

---
+++ MapApp-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapApp-SwiftUI
+//
+//  Created by Abdul Majid Shaikh on 15/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapApp-SwiftUI/MapApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp_SwiftUIApp.swift
+//  MapApp-SwiftUI
+//
+//  Created by Abdul Majid Shaikh on 15/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MapApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
